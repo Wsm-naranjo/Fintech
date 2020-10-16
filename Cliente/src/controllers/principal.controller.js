1ff526331f8b809f2276ff151e9cf85bacb2618a
Robin-Rivadeneira <48358672+Robin-Rivadeneira@users.noreply.github.com>
2020-10-16 11:45:14 -0500

    merge

--- Cliente/src/controllers/principal.controller.js
+++ Cliente/src/controllers/principal.controller.js
@@ -5,5 +5,6 @@ const pool = require('../database');
 principalController.rederList = async (req, res) => {
     const lista = await pool.query('SELECT * FROM Tienda WHERE user_id = ?', [req.user.id])
     res.render('pag-principal', { lista });
-};
+}
+
 module.exports = principalController;
